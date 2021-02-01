93ca7a2136bbdf52c7d2c0c08aac7db4824964cf
Eugenia Galantseva <lucyatsky@gmail.com>
2021-02-01 09:55:58 -0600

    created OrderForm component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+
+  return (
+      <div className='header w-100 h-10'>
+      <p>This is a header</p>
+      </div>
+      
+  );
+
+};
+
+export default Header;
